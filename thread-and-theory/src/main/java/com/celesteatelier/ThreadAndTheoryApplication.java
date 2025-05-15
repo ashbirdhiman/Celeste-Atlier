ef156a26bc3ddc5ef2b50fd07674db465e8c1b38
ashbirdhiman <ashbirashu@gmail.com>
2025-05-14 20:54:48 -0400

    add basic files login and Cart Microservice

---
+++ thread-and-theory/src/main/java/com/celesteatelier/ThreadAndTheoryApplication.java
@@ -0,0 +1,15 @@
+package com.celesteatelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ThreadAndTheoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadAndTheoryApplication.class, args);
+    }
+
+}
